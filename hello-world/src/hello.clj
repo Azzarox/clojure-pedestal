b6ad6c6e2feab16d39db31f963edbf4c786176d0
Azzarox <62171309+Azzarox@users.noreply.github.com>
2026-04-08 17:16:25 +0300

    Added step 2 from Pedestal documentation guide; For more information: https://pedestal.io/pedestal/0.8/guides/hello-world-query-parameters.html

--- hello-world/src/hello.clj
+++ hello-world/src/hello.clj
@@ -1,22 +1,76 @@
 (ns hello
   (:require [io.pedestal.connector :as conn]
-            [io.pedestal.http.http-kit :as hk]))
+            [io.pedestal.http.http-kit :as hk]
+            [clojure.string :as str]))
+
+(def unmentionables
+  #{"John" "George" "Michael"})
+
+(defn not-found
+  "Returns 404"
+  []
+  {:status 404 :body "Not Found!\n"})
+
+(defn ok
+  [message]
+  {:status 200 :body message})
+
+(defn greeting-for
+  [greet-name]
+  (let [trimmed (when greet-name (str/trim greet-name))]
+    (cond
+      (nil? trimmed) ;; We do not use (empty? because this checks for nil or "")
+      "Hello, world!\n"
+
+      (zero? (count trimmed)) ;; If we have query param but it is of 0 length 
+      "400 Bad Request!\n"
+
+      (unmentionables trimmed) nil
+
+      :else
+      (str "Hello, " trimmed "\n"))))
 
 (defn greet-handler
-  [_request]
-  {:body "Hello, world!\n"
-   :status 200})
+  [request]
+  (let [greet-name (get-in request [:query-params :name])
+        message    (greeting-for greet-name)]
+    (if message
+      (ok message)
+      (not-found))))
 
+;; Using #' it evaluates the Var and not the function behind it ... this way i dont need to evaluate depending functions and stuff when i change something
 (def routes
-  #{["/greet" :get greet-handler :route-name :greet]})
+  #{["/greet" :get #'greet-handler :route-name :greet]})
+
+(def port 8890)
 
 (defn create-connector
   []
-  (-> (conn/default-connector-map 8890)
+  (-> (conn/default-connector-map port)
       (conn/with-default-interceptors)
       (conn/with-routes routes)
       (hk/create-connector nil)))
 
+(defonce *connector (atom nil))
+
 (defn start
   []
-  (conn/start! (create-connector)))
+  (println "Server is starting...")
+  (reset! *connector
+          (conn/start! (create-connector)))
+  (println "Server is listening on PORT:" port))
+
+(defn stop
+  []
+  (println "Server is stopping...")
+  (conn/stop! @*connector)
+  (reset! *connector nil)
+  (println "Server stopped!"))
+
+(defn restart
+  []
+  (println "Server is restarting...")
+  (when @*connector
+    (stop))
+  (start)
+  (println "Server is ready!"))
